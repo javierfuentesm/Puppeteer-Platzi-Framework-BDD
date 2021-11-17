126088b4894fdcc2114528058ea9fb769d9c2e8d
Javier <javier.fuentesm@hotmail.com>
2021-11-16 21:28:07 -0500

    setup

---
+++ codecept.conf.js
@@ -0,0 +1,46 @@
+exports.config = {
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'https://www.phptravels.net/',
+      show: true,
+      windowSize: ''
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  timeout: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.js']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    }
+  },
+  stepTimeout: 0,
+  stepTimeoutOverride: [{
+      pattern: 'wait.*',
+      timeout: 0
+    },
+    {
+      pattern: 'amOnPage',
+      timeout: 0
+    }
+  ],
+  tests: './*_test.js',
+  name: 'FrameWorkPuppeteerBDD'
+}
